1803daa644201226a88fadeb3efd713c3f397ace
Raunak173 <raunakagarwal173@gmail.com>
2024-03-21 19:26:24 +0530

    mock api hosted

--- src/services/getModels.ts
+++ src/services/getModels.ts
@@ -1,11 +1,11 @@
 export const getModels=async()=> {
   try {
-    const response = await fetch('http://localhost:3001/models');
+    const response = await fetch('https://mocki.io/v1/48f3f4cc-c2d0-43dc-8118-48afac84e019');
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
     const data = await response.json();
-    return data;
+    return data?.models;
   } catch (error) {
     console.error('Error fetching data:', error);
   }
