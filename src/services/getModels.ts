e9db731305845c6051f97cc75d335eaec787d802
Raunak2311 <raunak.agarwal@sigiq.ai>
2024-07-16 19:34:51 +0530

    deploy new api

--- src/services/getModels.ts
+++ src/services/getModels.ts
@@ -1,12 +1,26 @@
 export const getModels=async()=> {
   try {
-    const response = await fetch('https://mocki.io/v1/48f3f4cc-c2d0-43dc-8118-48afac84e019');
+    const response = await fetch('https://aimodelize-server-production.up.railway.app/models');
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
     const data = await response.json();
-    return data?.models;
+    return data;
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
+
+export const getFaeturedModels=async()=> {
+  try {
+    const response = await fetch('https://aimodelize-server-production.up.railway.app/featured/models');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
